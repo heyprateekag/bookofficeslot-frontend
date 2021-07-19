01f9e1bfed453ff4694761c442f400f8f16c548d
Prateek <prateekag.998@gmail.com>
2021-07-20 00:45:02 +0530

    added dist in location

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require("express");
 const path = require("path");
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static("./src"));
-
+app.use(express.static(__dirname + "/dist/bookofficeslot"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "src/" });
+  res.sendFile(path.join(__dirname + "/dist/bookofficeslot/index.html"));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
