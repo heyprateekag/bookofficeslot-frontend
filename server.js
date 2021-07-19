cc8290ab748e9c152f002c25a2d13bbe29f5d796
Prateek <prateekag.998@gmail.com>
2021-07-20 00:30:35 +0530

    added src

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/bookofficeslot"));
+app.use(express.static("./dist/bookofficeslot/src"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/bookofficeslot/" });
+  res.sendFile("index.html", { root: "dist/bookofficeslot/src/" });
 });
 
 // Start the app by listening on the default Heroku port
